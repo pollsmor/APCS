fb211fe9c800412fba267fb40637b9c13bc6225a
Kevin Li <kevinli4321@gmail.com>
2019-01-31 20:12:41 -0500

    Test cases for sqrt

--- Driver.java
+++ Driver.java
@@ -1,5 +1,10 @@
 public class Driver {
   public static void main(String[] args) {
-    
+    System.out.println(recursion.sqrt(1, 0.0001));
+    System.out.println(recursion.sqrt(2, 0.0001));
+    System.out.println(recursion.sqrt(4, 0.0001));
+    System.out.println(recursion.sqrt(10000, 0.00000001));
+    System.out.println(recursion.sqrt(900, 0.0001));
+    System.out.println(recursion.sqrt(0, 0.0001));
   }
 }
