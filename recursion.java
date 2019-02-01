ebacb33c4b167ffa9ba3e0a3f2cc240ffb273fa8
Kevin Li <kevinli4321@gmail.com>
2019-01-31 20:13:26 -0500

    I can make the percError and guess fns. private

--- recursion.java
+++ recursion.java
@@ -1,5 +1,5 @@
 public class recursion {
-  public static double percError(double n, double guess) {
+  private static double percError(double n, double guess) {
     //guess is the square root while n is the square itself, so I have to square the guess
     return Math.abs((guess * guess - n) / n * 100);
   }
@@ -12,7 +12,7 @@ public class recursion {
     return guess(n, 1, tolerance); //initial guess is 1, can be whatever
   }
 
-  public static double guess(double n, double guess, double tolerance) {
+  private static double guess(double n, double guess, double tolerance) {
     //Given percent error
     if (percError(n, guess) < tolerance)
       return guess;
