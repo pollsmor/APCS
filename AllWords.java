8104cc75fe9281d321306a598b8814177bd386c4
Kevin Li <kevinli4321@gmail.com>
2019-02-04 14:37:04 -0500

    Set up everything

--- AllWords.java
+++ AllWords.java
@@ -1,1 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
 
+public class AllWords {
+  public static List<String> makeAllWords(int k, int maxLetter) {
+    List<String> output = new ArrayList<String>();
+    allWordsRec(k, "", output);
+    return output;
+  }
+
+  private static void allWordsRec(int k, String word, List<String> list) {
+
+  }
+}
---
+++ Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+  public static void main(String[] args) {
+    System.out.println(AllWords.makeAllWords(1, 3));
+    System.out.println(AllWords.makeAllWords(2, 3));
+    System.out.println(AllWords.makeAllWords(3, 3));
+  }
+}
