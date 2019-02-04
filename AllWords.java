e483d20e75a7fbc925e5625949c7dcc3868cd9b4
Kevin Li <kevinli4321@gmail.com>
2019-02-04 14:44:41 -0500

    Fixed said issue

--- AllWords.java
+++ AllWords.java
@@ -12,7 +12,8 @@ public class AllWords {
     if (k == 0)
       return;
 
-    list.add(word);
+    if (!word.equals(""))
+      list.add(word);
 
     for (int i = 97; i <= 97 + k; ++i)
       allWordsRec(k - 1, word + (char)i, list);
