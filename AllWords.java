d2628f792b2cb65278ee20d54543eb9c39ea3591
Kevin Li <kevinli4321@gmail.com>
2019-02-05 11:36:24 -0500

    Fixed issue of it printing a char ahead of z, added support above c

--- AllWords.java
+++ AllWords.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 public class AllWords {
   public static List<String> makeAllWords(int k, int maxLetter) {
     List<String> output = new ArrayList<String>();
-    allWordsRec(k, "", output, k);
+    allWordsRec(k, "", output, k, maxLetter);
     return output;
   }
 
-  private static void allWordsRec(int k, String word, List<String> list, int original) {
+  private static void allWordsRec(int k, String word, List<String> list, int original, int maxLetter) {
     if (k == -1)
       return;
 
     if (!word.equals("") && word.length() == original)
       list.add(word);
 
-    for (int i = 97; i <= 97 + k ; ++i)
-      allWordsRec(k - 1, word + (char)i, list, original);
+    for (int i = 97; i < 97 + maxLetter ; ++i)
+      allWordsRec(k - 1, word + (char)i, list, original, maxLetter);
   }
 }
--- Driver.java
+++ Driver.java
@@ -1,7 +1,11 @@
 public class Driver {
   public static void main(String[] args) {
     System.out.println(AllWords.makeAllWords(1, 26));
+    System.out.println();
     System.out.println(AllWords.makeAllWords(2, 26));
+    System.out.println();
     System.out.println(AllWords.makeAllWords(3, 3));
+    System.out.println();
+    System.out.println(AllWords.makeAllWords(4, 2));
   }
 }
