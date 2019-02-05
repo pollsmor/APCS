dd90a4be4aeae0aa224007be7939047f51355694
Kevin Li <kevinli4321@gmail.com>
2019-02-05 11:33:56 -0500

    Fixed to get the right amount of letters

--- AllWords.java
+++ AllWords.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 public class AllWords {
   public static List<String> makeAllWords(int k, int maxLetter) {
     List<String> output = new ArrayList<String>();
-    allWordsRec(k, "", output);
+    allWordsRec(k, "", output, k);
     return output;
   }
 
-  private static void allWordsRec(int k, String word, List<String> list) {
+  private static void allWordsRec(int k, String word, List<String> list, int original) {
     if (k == -1)
       return;
 
-    if (!word.equals("") && word.length == )
+    if (!word.equals("") && word.length() == original)
       list.add(word);
 
     for (int i = 97; i <= 97 + k ; ++i)
-      allWordsRec(k - 1, word + (char)i, list);
+      allWordsRec(k - 1, word + (char)i, list, original);
   }
 }
