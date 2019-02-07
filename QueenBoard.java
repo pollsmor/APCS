fde3e402ae23516c6ba5f960b06efaf4a73c2bd7
Kevin Li <kevinli4321@gmail.com>
2019-02-07 11:36:35 -0500

    addQueen fn.

--- QueenBoard.java
+++ QueenBoard.java
@@ -4,4 +4,13 @@ public class QueenBoard {
   public QueenBoard(int size) {
     board = new int[size][size];
   }
+
+  private boolean addQueen(int r, int c) {
+    if (board[r][c] != -1) {
+      board[r][c] = -1;
+      return true;
+    }
+
+    return false;
+  }
 }
