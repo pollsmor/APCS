3fc024bcce4cb4176623c142610871bddedd8e04
Kevin Li <kevinli4321@gmail.com>
2019-02-07 19:11:00 -0500

    Making add/removeQueen public for testing, will re-make private later

--- Driver.java
+++ Driver.java
@@ -1,6 +1,7 @@
 public class Driver {
   public static void main(String[] args) {
     QueenBoard test = new QueenBoard(8);
+    test.addQueen(0, 0);
     System.out.println(test);
   }
 }
--- QueenBoard.java
+++ QueenBoard.java
@@ -5,7 +5,7 @@ public class QueenBoard {
     board = new int[size][size];
   }
 
-  private boolean addQueen(int r, int c) {
+  public boolean addQueen(int r, int c) {
     if (board[r][c] != -1) {
       board[r][c] = -1;
       return true;
@@ -14,7 +14,7 @@ public class QueenBoard {
     return false;
   }
 
-  private boolean removeQueen(int r, int c) {
+  public boolean removeQueen(int r, int c) {
     if (board[r][c] == -1) {
       board[r][c] = 0;
       return true;
