725c11965bbb6019b2f4b0463547288929e77b66
Kevin Li <kevinli4321@gmail.com>
2019-02-10 09:01:03 -0500

    Copy paste addQueen to removeQueen

--- QueenBoard.java
+++ QueenBoard.java
@@ -42,6 +42,32 @@ public class QueenBoard {
 
   public boolean removeQueen(int r, int c) {
     if (board[r][c] == -1) {
+      for (int i = 1; i <= board.length - r; ++i) {
+        if (r != 0) //top right
+          --board[r - i][c + i];
+
+        if (c + i < board.length) //right
+          --board[r][c + i];
+
+        if (r + i < board.length && c + i < board.length) //bottom right
+          --board[r + i][c + i];
+
+        if (r + i < board.length) //bottom
+          --board[r + i][c];
+
+        if (r != 0) //top
+          --board[r - i][c];
+
+        if (r - i >= 0 && c - i >= 0) //top left
+          --board[r - i][c - i];
+
+        if (c - i >= 0) //left
+          --board[r][c - i];
+
+        if (r + i < board.length && c - i >= 0) //bottom left
+          --board[r + i][c - i];
+      }
+
       board[r][c] = 0;
       return true;
     }
