d92381a8c574ba90af28e2768ef4bbd87081ef39
Kevin Li <kevinli4321@gmail.com>
2019-02-10 11:10:01 -0500

    Tests

--- Driver.java
+++ Driver.java
@@ -2,10 +2,13 @@ public class Driver {
   public static void main(String[] args) {
     QueenBoard test = new QueenBoard(8);
     test.addQueen(4, 3);
+    test.removeQueen(4, 3);
     System.out.println(test);
 
     QueenBoard test2 = new QueenBoard(8);
     test2.addQueen(0, 0);
+    test2.removeQueen(1, 1);
+    test2.removeQueen(0, 0);
     System.out.println(test2);
   }
 }
