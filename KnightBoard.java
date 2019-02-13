53f6c0d22acc9f46bf53c39213f9d257de51a4f6
Kevin Li <kevinli4321@gmail.com>
2019-02-13 14:26:45 -0500

    Throws appropriate exception in constructor

--- KnightBoard.java
+++ KnightBoard.java
@@ -4,6 +4,9 @@ public class KnightBoard {
   private int cols;
 
   public KnightBoard(int startingRows, int startingCols) {
+    if (startingRows <= 0 || startingCols <= 0)
+      throw new IllegalArgumentException("It's not a board if it has <= 0 rows or cols.");
+
     rows = startingRows;
     cols = startingCols;
     board = new int[rows][cols];
