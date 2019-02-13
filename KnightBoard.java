25813f94a44721b83d7a5084bd5e4b6b7887c2cd
Kevin Li <kevinli4321@gmail.com>
2019-02-13 14:38:58 -0500

    Changed instance variables names to avoid conflicting with constructor varnames

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,28 +1,30 @@
 public class KnightBoard {
   private int[][] board;
-  private int rows;
-  private int cols;
-  private int row;
-  private int col;
+  private int m_rows;
+  private int m_cols;
+  private int m_row;
+  private int m_col;
 
-  public KnightBoard(int startingRows, int startingCols) {
+  public KnightBoard(int rows, int cols) {
     if (startingRows <= 0 || startingCols <= 0)
       throw new IllegalArgumentException("It's not a board if it has <= 0 rows or cols.");
 
-    rows = startingRows;
-    cols = startingCols;
-    board = new int[rows][cols];
+    m_rows = rows;
+    m_cols = cols;
+    board = new int[m_rows][m_cols];
   }
 
   public String toString() {
     if (board[0][0] != 0)
       throw new IllegalStateException("The board must be empty before you're allowed to do this.");
 
-    if (row <= 0 || row >= rows || col <= 0 || col >= cols)
+    if (m_row <= 0 || m_row >= m_rows || m_col <= 0 || m_col >= m_cols)
       throw new IllegalArgumentException("You can't go out of bounds.");
 
     String output = "";
 
     return output;
   }
+
+  public boolean solve(int startingRow, )
 }
