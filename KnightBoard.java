ca20ba210384ca6b1aa9a2651ae573fbf3381e5a
Kevin Li <kevinli4321@gmail.com>
2019-02-13 14:25:47 -0500

    Constructor

--- KnightBoard.java
+++ KnightBoard.java
@@ -2,4 +2,10 @@ public class KnightBoard {
   private int[][] board;
   private int rows;
   private int cols;
+
+  public KnightBoard(int startingRows, int startingCols) {
+    rows = startingRows;
+    cols = startingCols;
+    board = new int[rows][cols];
+  }
 }
