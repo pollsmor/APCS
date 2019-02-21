10bfcf9b5a4eefa9987712b36285aa14de5be577
Kevin Li <kevinli4321@gmail.com>
2019-02-20 20:14:57 -0500

    First try, damn (tests)

--- Driver.java
+++ Driver.java
@@ -1,8 +1,8 @@
 public class Driver {
   public static void main(String[] args) {
-    KnightBoard test = new KnightBoard(5, 5);
+    KnightBoard test = new KnightBoard(5, 6);
 
-    test.solve(0, 0);
+    System.out.println(test.countSolutions(0, 0));
 
     System.out.println(test);
   }
