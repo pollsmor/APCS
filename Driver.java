f2fd4d29dad9c5e1546d0ba82ff5fe41f58ca2ee
Kevin Li <kevinli4321@gmail.com>
2019-02-26 14:38:37 -0500

    Got code from stuycs website

--- Driver.java
+++ Driver.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class Driver {
   public static void main(String[] args) {
+    File text = new File("Maze1.txt");
+    Scanner inf = new Scanner(text);
 
+    while (inf.hasNextLine()) {
+      String line = inf.nextLine();
+      System.out.println(line);
+    }
   }
 }
