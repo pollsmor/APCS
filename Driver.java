226d8361bd72baf13f5053653059cb0ffa5f909f
Kevin Li <kevinli4321@gmail.com>
2019-02-26 14:46:56 -0500

    Read in the files in Try catch block

--- Driver.java
+++ Driver.java
@@ -4,12 +4,18 @@ import java.util.Scanner;
 
 public class Driver {
   public static void main(String[] args) {
-    File text = new File("Maze1.txt");
-    Scanner inf = new Scanner(text);
+    try {
+      File text = new File("Maze1.txt");
+      Scanner inf = new Scanner(text);
 
-    while (inf.hasNextLine()) {
-      String line = inf.nextLine();
-      System.out.println(line);
+      while (inf.hasNextLine()) {
+        String line = inf.nextLine();
+        System.out.println(line);
+      }
+    }
+
+    catch (FileNotFoundException e) {
+
     }
   }
 }
