d9774dfa2f550f2af6ad4f88b43cf8cb478949d3
Kevin Li <kevinli4321@gmail.com>
2019-02-27 13:52:43 -0500

    Constructor fixed

--- Maze.java
+++ Maze.java
@@ -4,36 +4,32 @@ import java.io.*;
 public class Maze {
   private char[][] maze;
   private boolean animate = false; //by default
-  private int rows;
+  private int rows = 0;
   private int cols;
 
   public Maze(String fileName) throws FileNotFoundException {
     File f = new File(fileName);
 
-    //cols in maze
-    Scanner colsChecker = new Scanner(text);
-    int cols = colsChecker.nextLine().length(); //length of one line, which is cols
-
     //rows in maze
-    Scanner inf2 = new Scanner(text);
-    int rows = 0; //count the amount of lines
-    while (inf2.hasNextLine()) {
+    Scanner rowsChecker = new Scanner(f);
+    while (rowsChecker.hasNextLine()) { //count the amount of lines
       ++rows;
-      inf2.nextLine();
+      rowsChecker.nextLine();
     }
 
-    //Read everything into char array
-    Scanner inf3 = new Scanner(text);
-    char[][] mazeEls = new char[rows][cols];
-    for (int i = 0; i < rows; ++i) {
-      String line = inf3.nextLine();
-      for (int j = 0; j < cols; ++j) {
-        mazeEls[i][j] = line.charAt(j);
-      }
+    //cols in maze
+    Scanner colsChecker = new Scanner(f);
+    cols = colsChecker.nextLine().length(); //length of one line, which is cols
 
-    maze = inputMaze;
-    rows = maze.length;
-    cols = maze[0].length;
+    //Read everything into char array
+    Scanner inf = new Scanner(f);
+    maze = new char[rows][cols];
+    for (int i = 0; i < rows; ++i) {
+      String line = inf.nextLine();
+      for (int j = 0; j < cols; ++j) {
+        maze[i][j] = line.charAt(j);
+      }
+    }
   }
 
   public String toString() {
