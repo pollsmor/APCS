3f298423d8f01f310519a818df139205aac0a0c0
Kevin Li <kevinli4321@gmail.com>
2019-02-26 20:39:02 -0500

    See amount of rows/cols in maze

--- Driver.java
+++ Driver.java
@@ -7,7 +7,15 @@ public class Driver {
     try {
       File text = new File("Maze1.txt");
       Scanner inf = new Scanner(text);
+      int rows = 0;
+      while (inf.hasNextLine()) {
+        ++rows;
+        inf.nextLine();
+      }
+      int cols = inf.nextLine().length();
+      char[][] maze = new char[rows][cols];
 
+      //Print out stuff
       while (inf.hasNextLine()) {
         String line = inf.nextLine();
         System.out.println(line);
