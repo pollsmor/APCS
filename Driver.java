47a8865dd1824e97363ea0630398f3f246c5ae98
Kevin Li <kevinli4321@gmail.com>
2019-02-26 21:57:57 -0500

    Read everything into array

--- Driver.java
+++ Driver.java
@@ -6,25 +6,26 @@ public class Driver {
   public static void main(String[] args) {
     try {
       File text = new File("Maze1.txt");
-      Scanner colsChecker = new Scanner(text);
+
       //cols in maze
-      int cols = colsChecker.nextLine().length();
+      Scanner colsChecker = new Scanner(text);
+      int cols = colsChecker.nextLine().length(); //length of one line, which is cols
 
       //rows in maze
       Scanner inf2 = new Scanner(text);
-      int rows = 0;
+      int rows = 0; //count the amount of lines
       while (inf2.hasNextLine()) {
         ++rows;
         inf2.nextLine();
       }
 
+      //Read everything into char array
       Scanner inf3 = new Scanner(text);
       char[][] mazeEls = new char[rows][cols];
       for (int i = 0; i < rows; ++i) {
         String line = inf3.nextLine();
         for (int j = 0; j < cols; ++j) {
-          char el = line.charAt(j);
-          mazeEls[i][j] = el;
+          mazeEls[i][j] = line.charAt(j);
         }
       }
 
