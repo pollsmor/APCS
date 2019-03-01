fddf5f80fdbcbd0c3b2e66214ca404dadf887b77
Kevin Li <kevinli4321@gmail.com>
2019-03-01 14:28:22 -0500

    atCount fn.

--- Maze.java
+++ Maze.java
@@ -103,7 +103,16 @@ public class Maze {
   }
 
   private int countAts() {
-    
+    int atCount = 0;
+
+    for (int i = 0; i < rows; ++i) {
+      for (int j = 0; j < cols; ++j) {
+        if (maze[i][j] == '@')
+          ++atCount;
+      }
+    }
+
+    return atCount;
   }
 
   private int solve(int row, int col) {
