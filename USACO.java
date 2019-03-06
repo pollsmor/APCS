8d73a6fb4bf269ee51f0d18db6f380651c3fa0d3
Kevin Li <kevinli4321@gmail.com>
2019-03-06 14:47:55 -0500

    Nearing end of class, note to self: don't use charAt

--- USACO.java
+++ USACO.java
@@ -10,13 +10,15 @@ public class USACO {
     int rows = Integer.parseInt(firstRow.next());
     int cols = Integer.parseInt(firstRow.next());
     int finalEl = Integer.parseInt(firstRow.next());
-    int instructions = Integer.parseInt(firstRow.next());
+    int instructionAmt = Integer.parseInt(firstRow.next());
 
     //Get data for the elevations of the map and the instructions
     int i = 1;
     Scanner inf = new Scanner(f);
     inf.nextLine(); //skip the first line of data since we got it already
     int[][] lake = new int[rows][cols];
+    int[][] instructions = new int[instructionAmt][3];
+
     while (inf.hasNextLine()) {
       if (i >= 1 && i <= rows) {
         String line = inf.nextLine();
@@ -25,8 +27,17 @@ public class USACO {
         }
       }
 
+      else if (i > rows && i <= rows + instructionAmt) {
+        String line = inf.nextLine();
+        for (int col = 0; col < 3; ++col) {
+          instructions[i - rows - 1][col] = line.charAt(col);
+        }
+      }
+
       ++i;
     }
+
+    System.out.println(printArr(lake));
 
     return 1;
   }
