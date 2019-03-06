4feb784ac0d7b99568ebef8db64b0eba8366f9d6
Kevin Li <kevinli4321@gmail.com>
2019-03-06 14:30:49 -0500

    Get needed data from first line of input

--- Driver.java
+++ Driver.java
@@ -1,5 +1,13 @@
+import java.util.*;
+import java.io.*;
+
 public class Driver {
   public static void main(String[] args) {
-    
+    if (args.length < 1) {
+      System.out.println("Please provide a file.");
+      System.exit(0);
+    }
+
+
   }
 }
--- USACO.java
+++ USACO.java
@@ -1,9 +1,26 @@
+import java.util.*;
+import java.io.*;
+
 public class USACO {
-  public static int bronze() {
+  public static int bronze(String fileName) throws FileNotFoundException {
+    File f = new File(fileName);
+
+    //Get data from the first line of input
+    Scanner firstRow = new Scanner(f);
+    int rows = Integer.parseInt(firstRow.next());
+    int cols = Integer.parseInt(firstRow.next());
+    int finalEl = Integer.parseInt(firstRow.next());
+    int instructions = Integer.parseInt(firstRow.next());
+
+    System.out.println(rows);
+    System.out.println(cols);
+    System.out.println(finalEl);
+    System.out.println(instructions);
+
     return 1;
   }
 
-  public static int silver() {
+  public static int silver(String fileName) {
     return 1;
   }
 }
