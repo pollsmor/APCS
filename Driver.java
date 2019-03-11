dcedd27666a988f86f77de520a4c47041c92c2aa
Kevin Li <kevinli4321@gmail.com>
2019-03-10 20:40:08 -0400

    Separated to be sorted areas from areas to be left alone

--- Driver.java
+++ Driver.java
@@ -1,5 +1,8 @@
 public class Driver {
   public static void main(String[] args) {
-    
+    int[] arr = {6, 7, 10, 91, 7, -9, 98421, -195};
+    int[] arr2 = {1, 2, 3, 4, 6, 7, 10, 91, 7, -9, 98421, -195};
+
+    Partition.partition(arr2, 3, 6);
   }
 }
--- Partition.java
+++ Partition.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class Partition {
-  public static int partition(int[][] data, int start, int end) {
+  public static int partition(int[] data, int start, int end) {
+    int[] leftSide = new int[start];
+    int[] toBeSorted = new int[end - start + 1];
+    int[] rightSide = new int[data.length - end - 1];
+
+    System.out.println(Arrays.toString(leftSide));
+    System.out.println(Arrays.toString(toBeSorted));
+    System.out.println(Arrays.toString(rightSide));
     return 1;
   }
 }
+
+//{6, 7, 10, 91, 7, -9, 98421, -195}
