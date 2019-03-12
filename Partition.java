d1497debf2f1b081c977dc4f428644011c71a9ed
Kevin Li <kevinli4321@gmail.com>
2019-03-11 20:19:38 -0400

    Actually generate random pivotIdx

--- Partition.java
+++ Partition.java
@@ -4,8 +4,23 @@ import java.util.Random;
 public class Partition {
   public static int partition(int[] data, int start, int end) {
     Random randgen = new Random();
-    int pivotIdx = Math.abs(randgen.nextInt() % (start + 1)) + start;
+    int pivotIdx = Math.abs(randgen.nextInt() % (end - start + 1)) + start;
     int pivot = data[pivotIdx];
+    System.out.println(pivotIdx);
+
+    int temp = data[start];
+    data[start] = pivot;
+    data[pivotIdx] = temp;
+    pivotIdx = 0;
+
+    for (int i = start; i <= end; ++i) {
+      if (data[i] < pivot) {
+        int temp2 = data[start];
+        data[start] = data[i];
+        data[i] = temp2;
+        ++pivotIdx;
+      }
+    }
 
     System.out.println(pivotIdx);
     System.out.println(Arrays.toString(data));
