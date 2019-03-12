70a2c3e6b5479a6ba403f71846b0f2ad177308fe
Kevin Li <kevinli4321@gmail.com>
2019-03-11 22:35:23 -0400

    Final comments

--- Partition.java
+++ Partition.java
@@ -2,20 +2,21 @@ import java.util.Random;
 
 public class Partition {
   public static int partition(int[] data, int start, int end) {
+    //In range between start and end, inclusive
     Random randgen = new Random();
     int pivotIdx = Math.abs(randgen.nextInt() % (end - start + 1)) + start;
-    int pivot = data[pivotIdx];
-    System.out.println(pivotIdx);
 
-    int i = start;
+    int pivot = data[pivotIdx];
+
+    int i = start; //don't really want to modify the actual start and end
     int j = end;
     while (i < j) {
-      while (data[i] < pivot) ++i;
-      while (data[j] > pivot) --j;
-      swap(data, i, j);
+      while (data[i] < pivot) ++i; //keep going up until something should be right of the pivot
+      while (data[j] > pivot) --j; //keep going down until something should be left of the pivot
+      swap(data, i, j); //swap the two indexes from earlier to get them onto the correct side
     }
 
-    return j;
+    return j; //new idx of the pivot
   }
 
   private static int[] swap(int[] data, int idx, int idx2) {
@@ -26,5 +27,3 @@ public class Partition {
     return data;
   }
 }
-
-//{1, 2, 3, 4, 6, 7, 10, 91, 7, -9, 98421, -195}
